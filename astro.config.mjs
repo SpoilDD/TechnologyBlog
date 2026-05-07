fbf9432a5183fc7b8a9a09d83c21571ddc9df0ee
海峰 <aidanspoil@gmail.com>
2026-05-07 11:26:55 +0800

    添加插件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://kiteaidan.vercel.app',
@@ -7,5 +9,7 @@ export default defineConfig({
       theme: 'github-dark-default',
       wrap: true,
     },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
